3f1376eb213299943024bae941cbfc77c79fa9a5
xqg <zhuofanxu@live.com>
2014-03-13 15:59:33 +0800

    two simple lua file

---
+++ factorial.lua
@@ -0,0 +1,12 @@
+--defines a factorial function
+function factorial (n)
+	if n == 0 then 
+		return 1
+	else 
+		return n * factorial(n-1)
+	end
+end
+
+print("enter a number:")
+a=io.read("*number")
+print(factorial(a))
